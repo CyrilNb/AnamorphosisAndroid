7c8bd21619c0714c7be22160866212aa1d7836e8
Cyril <cyril.niobe@gmail.com>
2018-04-25 23:13:58 +0200

    ADDED feature Load video from gallery

--- app/src/main/java/fr/univtln/group3/anamorphosisandroid/MainActivity.java
+++ app/src/main/java/fr/univtln/group3/anamorphosisandroid/MainActivity.java
@@ -1,21 +1,50 @@
 package fr.univtln.group3.anamorphosisandroid;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOAD_VIDEO_GALLERY_ACTIVITY_REQUEST_CODE = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+    Button btnLoadGallery;
+    VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        btnLoadGallery = (Button) findViewById(R.id.btnLoadGallery);
+        btnLoadGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoadFromGalleryButtonClicked(view);
+            }
+        });
+
+
 
     }
 
@@ -41,4 +70,75 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LOAD_VIDEO_GALLERY_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri selectedVideoUri = data.getData();
+            System.out.println(selectedVideoUri);
+
+            videoView.setVideoURI(selectedVideoUri);
+            videoView.start();
+
+            /*String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();*/
+
+
+
+            /*Bitmap loadedBitmap = BitmapFactory.decodeFile(picturePath);
+            mainImageView.setImageBitmap(loadedBitmap);
+            mainImageView.setAdjustViewBounds(true);
+            */
+
+        }
+
+
+    }
+
+    /**
+     * Runs when the gallery button is clicked from the bottom menu
+     * Performs the load of an image from the gallery
+     */
+    public void onLoadFromGalleryButtonClicked(View view) {
+        verifyStoragePermissions(MainActivity.this);
+        Intent galleryIntent = new Intent();
+        galleryIntent.setType("video/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        /*
+        Intent galleryIntent = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);*/
+
+        startActivityForResult(galleryIntent, LOAD_VIDEO_GALLERY_ACTIVITY_REQUEST_CODE);
+
+    }
+
+    /**
+     * If APK >= 23, we need to check at runtime for user permissions
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permissions required then the user will be prompted to grant permissions
+     *
+     * @param activity which performs the operation where permissions are requested
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if the application has write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // If not, prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
 }
