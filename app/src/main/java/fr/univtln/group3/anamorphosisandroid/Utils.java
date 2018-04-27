67e7ba316f85847dff7ab3e2842f7990fe515b1f
Cyril <cyril.niobe@gmail.com>
2018-04-27 11:23:30 +0200

    DELETE unused TODO

--- app/src/main/java/fr/univtln/group3/anamorphosisandroid/Utils.java
+++ app/src/main/java/fr/univtln/group3/anamorphosisandroid/Utils.java
@@ -39,7 +39,6 @@ public class Utils {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
                 }
 
-                // TODO handle non-primary volumes
             }
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
