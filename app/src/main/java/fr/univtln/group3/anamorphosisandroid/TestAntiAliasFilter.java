01f508925469cf2e1f0e2c467e3b2bd3353c0889
SimonAmil1 <simon-almicaro@etud.univ-tln.fr>
2018-06-01 11:47:33 +0200

    AsyncTask qui fait le traitement de l'image en appliquant un ou plusieurs filtre(s)

---
+++ app/src/main/java/fr/univtln/group3/anamorphosisandroid/TestAntiAliasFilter.java
@@ -0,0 +1,62 @@
+package fr.univtln.group3.anamorphosisandroid;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+/**
+ * Cette AsyncTask a servi de test
+ * et donc les opérations efféctuées ici pourront se faire dans l'AsyncTask principale
+ */
+public class TestAntiAliasFilter extends AsyncTask<Void,Bitmap,Void> {
+
+    ImageView imageViewResult;
+    //Canvas canvas;
+    Button btn;
+    ConvolutionFilter convolutionMatrix;
+
+    public TestAntiAliasFilter(ImageView imageView, Button btn){
+        imageViewResult = imageView;
+        this.btn = btn;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        //Log.d("doInBackground", "doInBackground");
+
+        /**
+         * Récupère la BitmapDrawable qui est dans l'ImageView
+         */
+        Bitmap bitmapResult = ((BitmapDrawable) imageViewResult.getDrawable()).getBitmap();
+
+
+        /**
+         * Ici on peut choisir quel algo d'anti-aliasing on veut appliquer à l'image (Bitmap)
+         */
+
+
+        /**
+         * On rafraîchit l'ImageView avec la même image qui a été modifié
+         */
+        publishProgress(bitmapResult);
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        btn.setEnabled(false);
+    }
+
+    @Override
+    protected void onProgressUpdate(Bitmap... bitmap) {
+        imageViewResult.setImageBitmap(bitmap[0]);
+    }
+}
